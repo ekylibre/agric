b8e5a112860e0d9223e429ab8fc5dfaa4c7058bb
Brice Texier <burisu@oneiros.fr>
2013-09-03 15:43:48 +0200

    Fully externalizes compile process and removes compass scoria

--- lib/agric.rb
+++ lib/agric.rb
@@ -1,20 +1,2 @@
 require "agric/version"
 require "agric/engine" if defined?(::Rails)
-# require 'compass'
-require 'pathname'
-
-module Agric
-
-  def self.root
-    Pathname.new(File.expand_path(__FILE__)).dirname.dirname
-  end
-
-  # def self.compass_extension_path
-  #   root.join("lib", "agric", "compass")
-  # end
-
-  # autoload :Compiler, 'agric/compiler'
-end
-
-# Compass registration
-# Compass::Frameworks.register('agric', :path => Agric.compass_extension_path)
--- lib/tasks/compiler.rb
+++
@@ -1,8 +0,0 @@
-require 'agric/compiler'
-
-desc "Compile font Agric #{Agric::VERSION}"
-task :compile do
-  Agric::Compiler.compile!
-end
-
-# task :build => :compile
